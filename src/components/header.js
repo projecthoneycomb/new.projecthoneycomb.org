5e27ead2367b9ff913d7f85e2a66b176cbdd8a13
James Williams <james@projecthoneycomb.org>
2019-08-30 11:11:26 +0100

    Initial Commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,31 +2,37 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./header.css"
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+  <header>
+    <div class="menu">
+      <div class="menu-item">
+        <a href="https://www.notion.so/projecthoneycomb/Interesting-Resources-708d8291637045e1bd43aa7e2d18e20a" target="_blank">
+          <span>Resources</span>
+        </a>
+      </div>
+      <div class="menu-item">
+        <Link>
+          <span>Get in touch</span>
         </Link>
-      </h1>
+      </div>
+      <div class="logo menu-item">
+        <svg class="logo-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 89"><g id="Group_1" transform="translate(-104.05 -56)"><path id="Path_37" class="st1" d="M123.03 106.61l2.29 1.43 19-11.14-.13-22.02L125.05 64l-19 11.14.13 22.02 10.71 5.79.07-2.81 2.19-1.46-8.08-4.37-.13-16.39 14.11-8.29 14.24 8.1.08 16.37-14.11 8.29-2.24 1.49"></path><path id="Path_38" class="st1" d="M144.17 94.4l-2.26-1.67-19 11.14.13 22.02 19.13 10.89 19-11.14-.13-22.02-9.57-5.29v2.38l-2.37 1.28 7.05 4.49.08 16.37-14.11 8.29-14.2-8.08-.13-16.39 14.18-8.6 2.19-1.28"></path></g></svg>
+        <Link>
+          <span style={{color: '#EA493D'}}>Project Honeycomb</span>
+        </Link>
+      </div>
+      <div class="menu-item">
+        <Link>
+          <span>Projects</span>
+        </Link>
+      </div>
+      <div class="menu-item">
+        <a href="https://identity.projecthoneycomb.org/login">
+          <span>Login</span>
+        </a>
+      </div>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,31 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import HeroImage from "../components/hero-image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <h1>Introducing Project Honeycomb</h1>
+    <h2>What we want to do, how we want to do it and where we need your help.</h2>
+    <div style={{ margin: `50px 0`, background: '#FBDCD9', borderRadius: 10 }}>
+      <HeroImage />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <p>As it stands, technology within schools is proving largely ineffectual. Teachers are finding that the hardware and software they are using is burdening them further in an environment where they are already stretched. On the other hand, students today are digital natives. They’re the first generation to have grown up with technology at their fingertips but their experiences in school have yet to match that of their personal lives.</p>
+    <p>We believe that by developing new and approachable software, alongside close collaboration from teachers, students and schools, we can improve the working and psychological lives of those involved in education. We have located three crucial areas for improvement that can drastically change the way students and teachers can communicate and use technology for the better.</p>
+    <h3>Creativity alongside technology, not in spite of it</h3>
+    <p>Historically, technology’s introduction to education has mainly focused on getting physical hardware into schools and was overwhelmingly unsuccessful. As Ex-Secretary of State for Education Damian Hinds put it, “In the past, Governments have been guilty of imposing unwanted technology on schools” with SMART boards, iPads, etc. In reality, for most of us, the technology we interact with on a day to day basis is software: apps, websites, presentations, documents, and that is where we believe the problem lies.</p>
+    <p>The relationship between schools and software appears to be at a breaking point as 63% of teachers say that tasks related to data inputting are a burden on their time due to duplicate data entry, unusable software and pointless, bureaucratic processes. In addition to that, only 36% of teachers believe that their training using new technology is adequate. </p>
+    <p>This imbalance for teachers can be vastly improved by the creation of better, more useful, software. Honeycomb is focusing on the reduction of relentless and often pointless data entry that restricts teachers from what they were trained to do; teach. Teachers are in their profession to teach and are often not able to, to the best of their ability, due to the sheer volume of time spent entering data on each individual child. We want to relieve the stress and workload of teachers, freeing them up to remain creative in their teaching.</p>
+    <h3>Finding the middle ground for the next generation of school software</h3>
+    <p>As we learn more about the landscape of software in schools we came across the</p>
+    <h3>Building a private space for mental wellbeing</h3>
+    <p>...</p>
+    <h3>Okay, but how are you going to do this?</h3>
+    <p>We don’t know yet. The people at Honeycomb have a commercial & research background, with a desire to help support education. We want to listen to you; students, teachers and leadership, and learn about the problems as you see them to try and solve them with you. We want to conduct a research phase of 3 months to help understand what you need and have your invaluable input in this process.</p>
+    <h3>How can I help?</h3>
+    <p>Firstly, it’s important that we don’t add more burden to your life if you’re a teacher, student, senior leader or just an interested parent, so you can choose how you want to talk to us and how much. We don’t want to, nor will we, abuse any time or contact information you give us.</p>
   </Layout>
 )
 
