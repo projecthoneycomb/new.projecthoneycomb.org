68e7eae3806d9fa239ba17d9329927707c1008db
James Williams <james@projecthoneycomb.org>
2019-10-30 13:38:33 +0000

    Create privacy.js

---
+++ src/pages/privacy.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => {
+
+  return <Layout>
+    <SEO title="Home" />
+    <h3>Project Looking Glass</h3>
+    <p>We don't store any data about you. At all.</p>
+  </Layout>
+}
+
+export default IndexPage
